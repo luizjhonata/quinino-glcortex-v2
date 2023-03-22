000c9b80181b295191fd56e720353816e318a248
Jhonata Luiz <jhonataluiz@hotmail.com>
2023-03-21 21:12:48 -0300

    project created

---
+++ src/main/java/com/luizjhonata/quininoglcortex/QuininoGlcortexApplication.java
@@ -0,0 +1,13 @@
+package com.luizjhonata.quininoglcortex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuininoGlcortexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuininoGlcortexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizjhonata/quininoglcortex/QuininoGlcortexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizjhonata.quininoglcortex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuininoGlcortexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

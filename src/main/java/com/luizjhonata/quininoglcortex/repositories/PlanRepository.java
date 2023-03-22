d12cfcfbcc8fbf8044163101215f98b387f3cdac
Jhonata Luiz <jhonataluiz@hotmail.com>
2023-03-21 21:47:41 -0300

    feat: plan and tariff repositories

---
+++ src/main/java/com/luizjhonata/quininoglcortex/repositories/PlanRepository.java
@@ -0,0 +1,7 @@
+package com.luizjhonata.quininoglcortex.repositories;
+
+import com.luizjhonata.quininoglcortex.models.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+}
---
+++ src/main/java/com/luizjhonata/quininoglcortex/repositories/TariffRepository.java
@@ -0,0 +1,7 @@
+package com.luizjhonata.quininoglcortex.repositories;
+
+import com.luizjhonata.quininoglcortex.models.Tariff;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TariffRepository extends JpaRepository<Tariff, Long> {
+}

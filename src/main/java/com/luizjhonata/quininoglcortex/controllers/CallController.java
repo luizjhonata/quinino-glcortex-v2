7c9c7181edbb18f24da68c5c9657e79999687727
Jhonata Luiz <jhonataluiz@hotmail.com>
2023-03-25 22:15:44 -0300

    fix: cors config

--- src/main/java/com/luizjhonata/quininoglcortex/controllers/CallController.java
+++ src/main/java/com/luizjhonata/quininoglcortex/controllers/CallController.java
@@ -17,6 +17,8 @@ public class CallController {
     }
 
     @Operation(summary = "Method to calculate costs of a call")
+    @CrossOrigin(origins = "https://luxury-peony-57e6bc.netlify.app/")
+//    @CrossOrigin(origins = "http://127.0.0.1:5173")
     @GetMapping
     public ResponseEntity<Call> callResponseEntity (String origin, String destiny, Double time, Long planId) throws RuntimeException {
         Call call = callService.calculateCallCost(origin, destiny, time, planId);
